86d4a29959a76fb251f8f2a3f9649dfb21a3e58b
LIONSHH <44923348+LIONSHH@users.noreply.github.com>
2020-06-28 14:02:06 -0500

    Added some more lines this is just a test

--- gitdemo.py
+++ gitdemo.py
@@ -1,2 +1,4 @@
-print ("Hello this is my first Demo git python Repo I am using pycharm and it;s fun")
-print ("Hello Welcome to my World')
+
+print ('HEllo by sam ')
+print('Welcome Everyone by samir')
+print("This is the local code stored on external Hard")
